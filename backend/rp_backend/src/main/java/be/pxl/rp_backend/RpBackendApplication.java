ddd134003ebbd0c572ed9451f155731c140d19ac
Mehmet Yavuz <yavuz_mehmet15@hotmail.com>
2021-05-07 23:17:55 +0200

    Add initial folders

---
+++ backend/rp_backend/src/main/java/be/pxl/rp_backend/RpBackendApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.rp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpBackendApplication.class, args);
+	}
+
+}
---
+++ backend/rp_backend/src/test/java/be/pxl/rp_backend/RpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.rp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
